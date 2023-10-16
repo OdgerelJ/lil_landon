562ab04be526bea55a26ba51dd051f7981c5570d
2D\odie.jamsranjav <odie.jamsranjav@slalom.com>
2023-10-16 14:48:51 -0500

    AWS test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React everyday
+          Learn React everyday deployed in AWS
         </a>
       </header>
     </div>

66c2d00bbb5998a7312172f77377866dd250ef8a
2D\odie.jamsranjav <odie.jamsranjav@slalom.com>
2023-10-17 11:03:52 -0500

    Added json to represent data and made components dynamic

---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Welcome from "./Welcome.js";
+import HotelInfo from "./Hotel-Info.js";
+
+const Main = () => {
+  return (
+    <main id="wrapper">
+      <Welcome />
+      <HotelInfo />
+    </main>
+  );
+};
+
+export default Main;
